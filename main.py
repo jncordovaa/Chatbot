7b022ffd3e323328788a2f179de70059d1b3a95a
Jhonnatan Córdova <jncordovaa@gmail.com>
2025-05-05 00:56:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ device     = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model      = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="auto",
+    low_cpu_men_usage = true,
     torch_dtype=torch.float16
 ).to(device)
 
